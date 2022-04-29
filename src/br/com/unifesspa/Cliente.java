de9c1197454c308e887f3f2237a4160d4297ab4b
alaimcosta <alaim.costa23@gmail.com>
2022-04-29 05:51:31 -0300

    Primeiro commit do projeto

---
+++ src/br/com/unifesspa/Cliente.java
@@ -0,0 +1,15 @@
+package br.com.unifesspa;
+
+import java.rmi.Naming;
+
+public class Cliente {
+	public static void main(String[] args) throws Exception {
+		String objName = "rmi://localhost:1099/Cont";
+		Counter cont = (Counter) Naming.lookup(objName);
+		//System.out.println("O novo valor do contador e:" +  cont.nextValue());
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(cont.nextValue());
+		}
+	}
+}
---
+++ src/br/com/unifesspa/Contador.java
@@ -0,0 +1,22 @@
+package br.com.unifesspa;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Contador extends UnicastRemoteObject implements Counter{
+
+	private int contador;
+
+	public Contador(int valorInicial) throws RemoteException {
+		this.contador = valorInicial;
+	}
+	
+	public int getValue() {
+		return this.contador;
+	}
+	
+	public int nextValue() {
+		return this.contador = this.contador+1;
+	}
+	
+}
